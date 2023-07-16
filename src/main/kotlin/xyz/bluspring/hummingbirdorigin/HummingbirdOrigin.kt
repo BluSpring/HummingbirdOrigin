4548cc484ce2e5c36fd2a3418d7fb185d1bb2e0a
Naz <bluspringindohq@gmail.com>
2023-07-16 15:05:00 +0800

    Initial stuff

---
+++ src/main/kotlin/xyz/bluspring/hummingbirdorigin/HummingbirdOrigin.kt
@@ -0,0 +1,9 @@
+package xyz.bluspring.hummingbirdorigin
+
+import net.fabricmc.api.ModInitializer
+
+class HummingbirdOrigin : ModInitializer {
+    override fun onInitialize() {
+
+    }
+}
---
+++ src/main/kotlin/xyz/bluspring/hummingbirdorigin/client/HummingbirdOriginClient.kt
@@ -0,0 +1,9 @@
+package xyz.bluspring.hummingbirdorigin.client
+
+import net.fabricmc.api.ClientModInitializer
+
+class HummingbirdOriginClient : ClientModInitializer {
+    override fun onInitializeClient() {
+
+    }
+}
